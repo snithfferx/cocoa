fcbf4353aa9c1d64c960925edeaeaf433eaf9026
Nav1 <snithfferx@outlook.com>
2025-07-11 00:06:42 -0600

    COCOA | Updating | Adding fonts folder for Jimp.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 // import node from '@astrojs/node';
-
 import netlify from '@astrojs/netlify';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify()
+  adapter: netlify({
+    edge: false,
+    includeFiles: [
+      path.resolve(__dirname, 'node_modules/@jimp/plugin-print/dist/fonts/**/*')
+    ]
+  })
 });
